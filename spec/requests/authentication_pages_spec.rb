f2563f6c259b7f0e20224b82ee17bf5635ced2e3
Chris Fernandez <chris2fernandez@gmail.com>
2014-04-06 18:03:12 -0500

    Start 9.4, Deleting users. All tests passing

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,14 +27,12 @@ describe "Authentication" do
       end
     end
 
-  describe "with valid information" do
+describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
+      it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
@@ -54,6 +52,22 @@ describe "Authentication" do
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
 
+        describe "when attempting to visit a protected page" do
+          before do
+            visit edit_user_path(user)
+            fill_in "Email",    with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+          end
+
+          describe "after signing in" do
+
+            it "should render the desired protected page" do
+              expect(page).to have_title('Edit user')
+            end
+          end
+        end
+
         describe "in the Users controller" do
 
           describe "visiting the edit page" do
@@ -66,7 +80,12 @@ describe "Authentication" do
             specify { expect(response).to redirect_to(signin_path) }
           end
         end
-      end
+
+          describe "visiting the user index" do
+            before { visit users_path }
+            it { should have_title('Sign in') }
+          end
+        end
     
   
  
