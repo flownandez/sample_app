c3de7586f9fc46769f18d6f7d5f7dc1974249d9a
Chris Fernandez <chris2fernandez@gmail.com>
2014-04-08 16:54:55 -0500

    Finish 9.6.1 - 9.6.9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,6 +21,9 @@ describe "Authentication" do
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
 
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
@@ -53,20 +56,35 @@ describe "Authentication" do
         let(:user) { FactoryGirl.create(:user) }
 
         describe "when attempting to visit a protected page" do
-          before do
-            visit edit_user_path(user)
-            fill_in "Email",    with: user.email
-            fill_in "Password", with: user.password
-            click_button "Sign in"
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
           end
 
-          describe "after signing in" do
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
 
-            it "should render the desired protected page" do
-              expect(page).to have_title('Edit user')
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
             end
           end
         end
+      end
+    
 
         describe "in the Users controller" do
 
